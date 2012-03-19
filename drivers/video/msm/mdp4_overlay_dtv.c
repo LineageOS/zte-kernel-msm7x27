9f88394be7d4b2661e59123325e5f0062d08ba65
Ravishangar Kalyanam <rkalya@codeaurora.org>
2012-03-19 16:10:42 -0700

    msm_fb: display: Enable timing generator during first update
    During first display update through pan_display or overlay_play,
    enable the timing generator for pull modes like LCDC, DSI Video
    and DTV interfaces
    
    Change-Id: Icec5b28f0df04a1223119222a1aad5129111f676
    Signed-off-by: Ravishangar Kalyanam <rkalya@codeaurora.org>
    
    Conflicts:
    
    	drivers/video/msm/mdp4.h
    	drivers/video/msm/mdp4_overlay_dsi_video.c

--- drivers/video/msm/mdp4_overlay_dtv.c
+++ drivers/video/msm/mdp4_overlay_dtv.c
@@ -56,6 +56,7 @@ static void __mdp_outp(uint32 port, uint32 value)
 
 static int first_pixel_start_x;
 static int first_pixel_start_y;
+static int dtv_enabled;
 
 static struct mdp4_overlay_pipe *dtv_pipe;
 
@@ -198,9 +199,6 @@ static int mdp4_dtv_start(struct msm_fb_data_type *mfd)
 	/* Test pattern 8 x 8 pixel */
 	/* MDP_OUTP(MDP_BASE + DTV_BASE + 0x4C, 0x80000808); */
 
-	/* enable DTV block */
-	MDP_OUTP(MDP_BASE + DTV_BASE, 1);
-	mdp_pipe_ctrl(MDP_OVERLAY1_BLOCK, MDP_BLOCK_POWER_ON, FALSE);
 	/* MDP cmd block disable */
 	mdp_pipe_ctrl(MDP_CMD_BLOCK, MDP_BLOCK_POWER_OFF, FALSE);
 
@@ -215,6 +213,7 @@ static int mdp4_dtv_stop(struct msm_fb_data_type *mfd)
 	/* MDP cmd block enable */
 	mdp_pipe_ctrl(MDP_CMD_BLOCK, MDP_BLOCK_POWER_ON, FALSE);
 	MDP_OUTP(MDP_BASE + DTV_BASE, 0);
+	dtv_enabled = 0;
 	/* MDP cmd block disable */
 	mdp_pipe_ctrl(MDP_CMD_BLOCK, MDP_BLOCK_POWER_OFF, FALSE);
 	mdp_pipe_ctrl(MDP_OVERLAY1_BLOCK, MDP_BLOCK_POWER_OFF, FALSE);
@@ -375,6 +374,7 @@ int mdp4_overlay_dtv_unset(struct msm_fb_data_type *mfd,
 		return result;
 
 	pipe->flags &= ~MDP_OV_PLAY_NOWAIT;
+	mdp4_overlay_reg_flush(pipe, 0);
 	mdp4_overlay_dtv_ov_done_push(mfd, pipe);
 
 	if (pipe->mixer_stage == MDP4_MIXER_STAGE_BASE &&
@@ -474,12 +474,23 @@ static void mdp4_overlay_dtv_wait4_ov_done(struct msm_fb_data_type *mfd,
 	mdp_disable_irq(MDP_OVERLAY1_TERM);
 }
 
+void mdp4_overlay_dtv_start(void)
+{
+	if (!dtv_enabled) {
+		mdp_pipe_ctrl(MDP_CMD_BLOCK, MDP_BLOCK_POWER_ON, FALSE);
+		/* enable DTV block */
+		MDP_OUTP(MDP_BASE + DTV_BASE, 1);
+		mdp_pipe_ctrl(MDP_OVERLAY1_BLOCK, MDP_BLOCK_POWER_ON, FALSE);
+		mdp_pipe_ctrl(MDP_CMD_BLOCK, MDP_BLOCK_POWER_OFF, FALSE);
+		dtv_enabled = 1;
+	}
+}
+
 void mdp4_overlay_dtv_ov_done_push(struct msm_fb_data_type *mfd,
 			struct mdp4_overlay_pipe *pipe)
 {
 	mdp4_overlay_reg_flush(pipe, 0);
 	mdp4_overlay_dtv_ov_start(mfd);
-
 	if (pipe->flags & MDP_OV_PLAY_NOWAIT)
 		return;
 
@@ -612,7 +623,6 @@ void mdp4_dtv_overlay_blt_stop(struct msm_fb_data_type *mfd)
 void mdp4_dtv_overlay(struct msm_fb_data_type *mfd)
 {
 	struct mdp4_overlay_pipe *pipe;
-
 	if (!mfd->panel_power_on)
 		return;
 
@@ -631,6 +641,8 @@ void mdp4_dtv_overlay(struct msm_fb_data_type *mfd)
 		mdp4_overlay_rgb_setup(pipe);
 	}
 	mdp4_mixer_stage_up(pipe);
+	mdp4_overlay_reg_flush(pipe, 0);
+	mdp4_overlay_dtv_start();
 	mdp4_overlay_dtv_ov_done_push(mfd, pipe);
 	mdp4_iommu_unmap(pipe);
 	mutex_unlock(&mfd->dma->ov_mutex);
