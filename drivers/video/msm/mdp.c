b8a07dd242d1ad1bf4b7a67d76f309cd8ae2765e
Tom Giordano <tomgiordano83@gmail.com>
2012-06-10 15:29:16 +1000

    fix-up
    Change-Id: I4d113c49be2d0d4129e6338e26df67c683d1556a

--- drivers/video/msm/mdp.c
+++ drivers/video/msm/mdp.c
@@ -1988,39 +1988,35 @@ static int mdp_probe(struct platform_device *pdev)
 		break;
 
 	case MIPI_CMD_PANEL:
-		mfd->dma_fnc = mdp4_dsi_cmd_overlay;
-		mipi = &mfd->panel_info.mipi;
-		configure_mdp_core_clk_table((mipi->dsi_pclk_rate) * 3 / 2);
-		if (mfd->panel_info.pdest == DISPLAY_1) {
-			if_no = PRIMARY_INTF_SEL;
-			mfd->dma = &dma2_data;
-		} else {
-			if_no = SECONDARY_INTF_SEL;
-			mfd->dma = &dma_s_data;
-		}
-		mfd->lut_update = mdp_lut_update_nonlcdc;
-		mfd->do_histogram = mdp_do_histogram;
-		mdp4_display_intf_sel(if_no, DSI_CMD_INTF);
-
-		mdp_pipe_ctrl(MDP_CMD_BLOCK, MDP_BLOCK_POWER_ON, FALSE);
-		spin_lock_irqsave(&mdp_spin_lock, flag);
-		mdp_intr_mask |= INTR_OVERLAY0_DONE;
-		outp32(MDP_INTR_ENABLE, mdp_intr_mask);
-		spin_unlock_irqrestore(&mdp_spin_lock, flag);
-		mdp_pipe_ctrl(MDP_CMD_BLOCK, MDP_BLOCK_POWER_OFF, FALSE);
-#else
-		mfd->dma_fnc = mdp_dma2_update;
-		mfd->do_histogram = mdp_do_histogram;
-		if (mfd->panel_info.pdest == DISPLAY_1)
-			mfd->dma = &dma2_data;
-		else {
-			printk(KERN_ERR "Invalid Selection of destination panel\n");
-			rc = -ENODEV;
-			goto mdp_probe_err;
-		}
+#ifndef CONFIG_FB_MSM_MDP303
+                mfd->dma_fnc = mdp4_dsi_cmd_overlay;
+#ifdef CONFIG_FB_MSM_MDP40
+                mipi = &mfd->panel_info.mipi;
+                configure_mdp_core_clk_table((mipi->dsi_pclk_rate) * 3 / 2);
 #endif
-		mdp_config_vsync(mfd);
-		break;
+                if (mfd->panel_info.pdest == DISPLAY_1) {
+                        if_no = PRIMARY_INTF_SEL;
+                        mfd->dma = &dma2_data;
+                } else {
+                        if_no = SECONDARY_INTF_SEL;
+                        mfd->dma = &dma_s_data;
+                }
+                mfd->lut_update = mdp_lut_update_nonlcdc;
+                mfd->do_histogram = mdp_do_histogram;
+                mdp4_display_intf_sel(if_no, DSI_CMD_INTF);
+#else
+                mfd->dma_fnc = mdp_dma2_update;
+                mfd->do_histogram = mdp_do_histogram;
+                if (mfd->panel_info.pdest == DISPLAY_1)
+                        mfd->dma = &dma2_data;
+                else {
+                        printk(KERN_ERR "Invalid Selection of destination panel\n");
+                        rc = -ENODEV;
+                        goto mdp_probe_err;
+                }
+#endif
+                mdp_config_vsync(mfd);
+                break;
 #endif
 
 #ifdef CONFIG_FB_MSM_DTV
